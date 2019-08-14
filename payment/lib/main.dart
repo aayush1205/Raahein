3179e1128080525d5dbf738c436acd64fdca24ef
aayush1205 <upadhyayaayush12@gmail.com>
2019-08-14 23:59:30 +0530

    Formatted final payment system

--- payment/lib/main.dart
+++ payment/lib/main.dart
@@ -4,19 +4,14 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/services.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 
-
-
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Raahein',
       theme: ThemeData(
-  
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Raahein Payment Page'),
@@ -27,7 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -35,9 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-
   String result = "Hey there !";
-
 
   Future _scanQR() async {
     try {
@@ -72,113 +64,86 @@ class MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    
-
     super.initState();
     _razorpay = Razorpay();
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
-
   }
 
-@override
-  void dispose() { 
-    super.dispose();      
+  @override
+  void dispose() {
+    super.dispose();
     _razorpay.clear();
   }
 
   void openCheckout() async {
     var options = {
-      'key' : 'rzp_test_XyoHRw6LGnq4bz',
-      'amount' : totalAmount*100,
+      'key': 'rzp_test_XyoHRw6LGnq4bz',
+      'amount': totalAmount * 100,
       'name': "Ministry of Trasnportation",
-      'description' : 'Test Pay',
-      'prefil' : {'contact': '', 'email': ''},
-      'external' : {
-        'wallets' : ['paytm']
+      'description': 'Test Pay',
+      'prefil': {'contact': '', 'email': ''},
+      'external': {
+        'wallets': ['paytm']
       }
     };
 
-    try{
+    try {
       _razorpay.open(options);
-    }catch(e){
+    } catch (e) {
       debugPrint(e);
     }
-
   }
 
-void _handlePaymentSuccess(PaymentSuccessResponse response){
-  Fluttertoast.showToast(msg: "SUCCESS" + response.paymentId);
-}
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(msg: "SUCCESS" + response.paymentId);
+  }
 
-void _handlePaymentError(PaymentFailureResponse response){
-  Fluttertoast.showToast(msg: "ERROR" + response.code.toString() + " - " + response.message );
-}
+  void _handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: "ERROR" + response.code.toString() + " - " + response.message);
+  }
 
-void _handleExternalWallet(ExternalWalletResponse response){
-  Fluttertoast.showToast(msg: "EXTERNAL WALLET" + response.walletName);
-}
-
-
-  
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(msg: "EXTERNAL WALLET" + response.walletName);
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             LimitedBox(
               maxWidth: 150.0,
               child: TextField(
                 keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  hintText: "Please enter the amount!"
-                ),
-                onChanged: (value){
+                decoration:
+                    InputDecoration(hintText: "Please enter the amount!"),
+                onChanged: (value) {
                   setState(() {
                     totalAmount = num.parse(value);
                   });
                 },
-              
               ),
             ),
             SizedBox(
               height: 15.0,
-              
             ),
           ],
         ),
       ),
-
       floatingActionButton: FloatingActionButton.extended(
-      icon: Icon(Icons.camera_alt),
-      label: Text("Scan"),
-      onPressed: _scanQR,
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, 
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-
-
-      
-
-    
   }
 }
-            
-
-
-
-
-
-
-              
