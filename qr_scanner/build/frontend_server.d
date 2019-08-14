c0df7275398639d05469a8e2bb3dfe780f5de18c
MANASWITA TYAGI <manaswitatyagi@MANASWITAs-MacBook-Air.local>
2019-08-14 17:18:10 +0530

    Adding QR

---
+++ qr_scanner/build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/manaswitatyagi/Desktop/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
---
+++ qr_scanner/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <barcode_scan/BarcodeScanPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BarcodeScanPlugin registerWithRegistrar:[registry registrarForPlugin:@"BarcodeScanPlugin"]];
+}
+
+@end
