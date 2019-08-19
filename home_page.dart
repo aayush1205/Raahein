5182e7b69c730c38d98e445418992c047c84eaf9
ayush-i <41798154+ayush-i@users.noreply.github.com>
2019-08-20 02:26:28 +0530

    Add files via upload
    History

---
+++ home_page.dart
@@ -0,0 +1,248 @@
+import "package:flutter/material.dart";
+import 'package:login_page_with_indicator_slider/triangle_painter.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Padding(
+            padding: MediaQuery.of(context).viewInsets,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: constraints.maxHeight,
+                  minHeight: constraints.maxHeight,
+                ),
+                child: HomePageBody(),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HomePageBody extends StatefulWidget {
+  @override
+  HomePageBodyState createState() => HomePageBodyState();
+}
+
+class HomePageBodyState extends State<HomePageBody>
+    with SingleTickerProviderStateMixin {
+  
+
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController?.dispose();
+    super.dispose();
+  }
+
+  Widget _buildHeaderPanel(BuildContext context) {
+    return Expanded(
+      flex: 0,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+         
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 60.0),
+                    child: Text(
+                      "RIDE HISTORY",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontFamily: "Nunito"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onSignInButtonPress() {
+    _pageController.animateToPage(0,
+        duration: Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  void _onSignUpButtonPress() {
+    _pageController?.animateToPage(1,
+        duration: Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  Widget _buildMenuBar(BuildContext context) {
+    return Container(
+      height: 70.0,
+      color: Color(0xFF000000),
+      child: CustomPaint(
+        painter: TrianglePainter(_pageController),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: _onSignInButtonPress,
+                child: Text(
+                  "Active Ride",
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                ),
+              ),
+            ),
+            Container(height: 33.0, width: 1.0, color: Colors.white),
+            Expanded(
+              child: FlatButton(
+                onPressed: _onSignUpButtonPress,
+                child: Text(
+                  "Past Rides",
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignIn(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 23.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0, top: 8.0, right: 120.0),
+            child: Text(
+              "CURRENT RIDE :",
+              style: TextStyle(fontSize: 20.0, fontFamily: "Nunito"),
+            ),
+          ),
+          
+          new Card(
+            color: Colors.black,
+                child: new Container(
+                  height: 100.0,
+                  width: 300.0,
+                  padding: new EdgeInsets.all(20.0),
+                  
+                   child: new Row(
+                    children: <Widget>[
+                      
+                      new Icon(Icons.directions_bus, color: Colors.white, size: 50.0,),
+                      
+                      new Text('Hello', 
+                      style: TextStyle(fontFamily: "Nunito", color: Colors.white),
+                      ),
+                    
+                    ],
+                  ),
+                
+                ),
+              ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignUp(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 23.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0, top: 8.0, right: 150.0),
+          child: Text(
+            "PAST RIDES :",
+            style: TextStyle(fontSize: 20.0, fontFamily: "Nunito"),
+          ),
+          ),
+          
+          new Card(
+            color: Colors.black,
+                child: new Container(
+                  height: 100.0,
+                  width: 300.0,
+                  padding: new EdgeInsets.all(20.0),
+                  
+                   child: new Row(
+                    children: <Widget>[
+                      
+                      new Icon(Icons.directions_bus, color: Colors.white, size: 50.0,),
+                      
+                      new Text('Hello', 
+                      style: TextStyle(fontFamily: "Nunito", color: Colors.white),
+                      ),
+                    
+                    ],
+                  ),
+                
+                ),
+              ),
+
+              new Card(
+            color: Colors.black,
+                child: new Container(
+                  height: 100.0,
+                  width: 300.0,
+                  padding: new EdgeInsets.all(20.0),
+                  
+                   child: new Row(
+                    children: <Widget>[
+                      
+                      new Icon(Icons.directions_bus, color: Colors.white, size: 50.0,),
+                      
+                      new Text('Hello', 
+                      style: TextStyle(fontFamily: "Nunito", color: Colors.white),
+                      ),
+                    
+                    ],
+                  ),
+                
+                ),
+              ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _buildHeaderPanel(context),
+        _buildMenuBar(context),
+        Expanded(
+          flex: 2,
+          child: PageView(
+            controller: _pageController,
+            children: <Widget>[
+              _buildSignIn(context),
+              _buildSignUp(context),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- main.dart
+++ main.dart
@@ -1,118 +1,17 @@
-//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import './FirstPage.dart' as first;
-import './SecondPage.dart' as second;
-import './ThirdPage.dart' as third;
+import 'package:login_page_with_indicator_slider/home_page.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: new ThemeData (primaryColor: Colors.black,),
-      home: new MyTabs(),
-    );
-  }
-}
-
-class MyTabs extends StatefulWidget {
-  @override
-  MyTabsState createState() => new MyTabsState();
-}
-
-class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
-
-
-  TabController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = new TabController(vsync: this, length: 3);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-      backgroundColor: Colors.transparent,
-      title: new Text("Hi Ayushi!"),
-      //elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
-      ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: new Text("Ayushi Saini"),
-              accountEmail: new Text("ayushi.21k06@gmail.com"),
-              currentAccountPicture: new CircleAvatar(
-                backgroundColor: Colors.white,
-                child: new Text("A"),
-              ),
-            ),
-            new ListTile(
-               title: new Text("Navigate"),
-               
-               trailing: new Icon(Icons.navigation),
-            ),
-            new Divider(),
-            new ListTile(
-               title: new Text("Share my ride"),
-               trailing: new Icon(Icons.share),
-            ),
-            new Divider(),
-            new ListTile(
-               title: new Text("Notifications"),
-               trailing: new Icon(Icons.notifications),
-            ),
-            new Divider(),
-            new ListTile(
-               title: new Text("Arcade"),
-               trailing: new Icon(Icons.games),
-            ),
-            new Divider(),
-            new ListTile(
-               title: new Text("Rewards"),
-               trailing: new Icon(Icons.card_giftcard),
-            ),
-          new Divider(),
-            new ListTile(
-               title: new Text("Settings"),
-               trailing: new Icon(Icons.settings),
-            ),
-          new Divider(),
-          ],
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        accentColor: Colors.black,
+        primaryColor: Color(0xFF000000),
+        iconTheme: IconThemeData(
+          color: Color(0xFF000000),
         ),
       ),
-      bottomNavigationBar: new Material(
-        color: Colors.black,
-        child: new TabBar(
-          controller: controller,
-          tabs: <Tab>[
-            new Tab(icon: new Icon(Icons.swap_horizontal_circle)),
-            new Tab(icon: new Icon(Icons.swap_horizontal_circle)),
-            new Tab(icon: new Icon(Icons.swap_horizontal_circle)),
-          ]
-        )
-      ),
-      body: new TabBarView(
-        controller: controller,
-        children: <Widget>[
-          new first.First(),
-          new second.Second(),
-          new third.Third()
-        ]
-      )
-    );
-  }
+      home: HomePage(),
+    ),
+  );
 }
-
-
