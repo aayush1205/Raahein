1d7f6445fdf986c2b2c1f276721d586fc365d7c8
ayush-i <ayushi.21k06@gmail.com>
2019-08-22 13:22:25 +0530

    Bottom Sheet

--- appui/lib/main.dart
+++ appui/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'bottom.dart';
+import 'tbottom.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,19 +10,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Transport',
+      
       theme: ThemeData(
         
-        primarySwatch: Colors.yellow
+        primaryColor: Colors.black
       ),
       home: MyHomePage(title: 'TRANSPORT'),
     );
   }
 }
 
+
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
- 
+  
 
   final String title;
 
@@ -33,33 +38,155 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title, style: TextStyle(fontFamily: "Nunito", fontSize: 20.0),),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            RaisedButton(
-              color: Colors.black,
-              child: Text('Combo', style: TextStyle(fontFamily:"Nunito", color: Colors.white),),
-              onPressed: () => _onAlertWithCustomContentPressed(context),
-            ),
-            RaisedButton(
-              color: Colors.black,
-              child: Text('Request Combo', style: TextStyle(fontFamily:"Nunito", color: Colors.white),),
-              onPressed: () => _onAlertWithCustomContentPressed(context),
-            ),
-          ],
-        ),
-      ),
-    );
+     
+     return Scaffold(
+       appBar: AppBar(
+         title: Text("TRANSPORT",style: TextStyle(color: Colors.white,fontFamily: "Nunito")),
+         actions: <Widget>[
+           IconButton(
+             icon: Icon(Icons.location_searching,
+             color: Colors.white,),
+             onPressed: (){},
+           )
+         ],
+         centerTitle: true,
+       ),
+       body: Container(
+         child: Stack(
+           children: <Widget> [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child:Padding(
+                          padding: EdgeInsets.only(top: 380.0),
+                        child: Bottomm(
+                          onPressed: () => _onAlertWithCustomContentPressed(context),
+                          child: Row(
+                            
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.only(right: 10, left: 10),
+                      child: new Image.asset("assets/iauto.png",
+                      width: 40,
+                      height: 40,
+                      ),
+                      
+                      /*Icon(Icons.directions_bus, 
+                      color: Colors.white, 
+                      size: 30.0,),*/
+                      ),
+                      
+
+                          Container(
+                            padding: EdgeInsets.only(right: 5, top:5, left:5),
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: Text('COMBO', 
+                                  style: TextStyle(fontFamily: "Nunito", 
+                                  color: Colors.black),
+                                  )
+                                  
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(0),
+                                  child: Text('Rs: 54', 
+                                  style: TextStyle(fontFamily: "Nunito", 
+                                  color: Colors.black),
+                                  )
+                                  
+                                ),
+                              
+                              ],
+                            ),
+                          ),
+
+                          
+                          
+                          Container(
+                            padding: EdgeInsets.only(right: 5, top:5, left:146),
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(top: 10),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Text('1-3pax', 
+                                  style: TextStyle(fontFamily: "Nunito", 
+                                  color: Colors.black,
+                                  fontSize: 12),
+                                      ),
+                                      Icon(Icons.person,
+                                      size: 15.0,
+                                      color: Colors.black
+                                      )
+                                    ],
+                                  
+                                  )
+                                  
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(0),
+                                  child: Text('ETA: 8 min', 
+                                  style: TextStyle(fontFamily: "Nunito", 
+                                  color: Colors.black),
+                                  )
+                                  
+                                ),
+                              
+                              ],
+                            ),
+                          ),
+                        ]
+                      ),
+                      )
+                      )
+                      ),
+                      Container(
+                           height: 40,
+                           width: 500,
+                           color: Colors.yellow[300],
+                           child: Padding(
+                             padding: EdgeInsets.only(left: 80, top:10),
+                           child: Text("MEDIUM TRAFFIC IN YOUR AREA",
+                           style: TextStyle(fontFamily: "Nunito",
+                           color: Colors.black) 
+                           )
+                          )      
+                        ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom:0.0),
+                        child: Bottommm(
+                          
+                          onPressed: () => _onAlertWithCustomContenttPressed(context),
+                          child: Row(
+                            
+                    children: <Widget>[
+                      Container(
+                      padding: EdgeInsets.only(left: 108, right: 108),
+                       child: Text(
+                         "Request Combo",
+                         style: TextStyle(color: Colors.white,
+                         fontSize: 20),
+                      ),
+                      )
+                    ]
+                          ),
+                        )
+                        )
+                      )
+                    ]
+              )
+           ]
+         )
+       )
+     );
   }
+    
   _onAlertWithCustomContentPressed(context) {
     Alert(
         context: context,
@@ -72,9 +199,9 @@ class _MyHomePageState extends State<MyHomePage> {
                  Expanded(
                  child:new Column(
                    children: <Widget>[
-                     new Text("Auto: Rs10", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
-                     new Text("Bus: Rs20", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
-                     new Text("Auto: Rs10", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
+                     new Text("Auto: Rs11", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
+                     new Text("Bus: Rs30", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
+                     new Text("Auto: Rs13", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
                    ],
                  )
                  )
@@ -86,7 +213,39 @@ class _MyHomePageState extends State<MyHomePage> {
           DialogButton(
             onPressed: () => Navigator.pop(context),
             child: Text(
-              "PAY",
+              "BACK",
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+          )
+        ]).show();
+  }
+  _onAlertWithCustomContenttPressed(context) {
+    Alert(
+        context: context,
+        title: "CONGRATULATIONS!",
+        image: Image.asset("assets/combo.png"),
+        content: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                 Expanded(
+                 child:new Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: <Widget>[
+                     new Text("Your ride has been booked!", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
+                     new Text("Arvind is arriving in 10min.", style: TextStyle(fontFamily: "Nunito", fontSize: 16.0)),
+                   ],
+                 )
+                 )
+              ],
+              )
+          ],
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text(
+              "OK",
               style: TextStyle(color: Colors.black, fontSize: 16),
             ),
           )
