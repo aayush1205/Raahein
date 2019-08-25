1cf965c0e4ae5ad003a7ef456a7dcb85a04d2767
aayush1205 <upadhyayaayush12@gmail.com>
2019-08-25 09:41:58 +0530

    Conducter App

--- firestore_crud/bus_list/ios/Flutter/flutter_export_environment.sh
+++ firestore_crud/bus_list/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/aayush/flutter_n/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/aayush/firestore_crud"
+export "FLUTTER_APPLICATION_PATH=/home/aayush/firestore_crud/bus_list"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
